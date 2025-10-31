9d88b6381580e619030a5b3cdabeba41cdad58ca
msidiq2235 <muhsidiq2005@gmail.com>
2025-10-31 14:57:18 +0700

    menambah variabel state untuk input FormDataDiri

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -15,5 +15,12 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf(value = "")}
+    var textAlamat by remember { mutableStateOf(value = "")}
+    var textJK by remember { mutableStateOf(value = "" )}
 
+    var nama by remember { mutableStateOf(value = "")}
+    var alamat by remember { mutableStateOf(value = "")}
+    var jenis by remember { mutableStateOf(value = "")}
 }
