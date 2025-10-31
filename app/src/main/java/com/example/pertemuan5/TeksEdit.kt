51881c863b4741c734d0640585e17118edd889ba
msidiq2235 <muhsidiq2005@gmail.com>
2025-10-31 14:56:39 +0700

    membuat kerangka Composable FormDataDiri

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -1,2 +1,19 @@
 package com.example.pertemuan5
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+
+}
