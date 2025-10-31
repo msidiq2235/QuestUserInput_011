cf522a761b2892bed63908c5bc3746ed11ca0c8f
msidiq2235 <muhsidiq2005@gmail.com>
2025-10-31 15:24:06 +0700

    menambah Divider di bawah input Alamat

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -6,15 +6,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -66,6 +71,14 @@ fun FormDataDiri(modifier: Modifier
                 onValueChange = {
                     textAlamat = it
                 }
+            )
+
+            Divider(
+                modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium), top = dimensionResource(
+                    id = R.dimen.padding_medium
+                )),
+                thickness = dimensionResource(id = R.dimen.divider_tipis),
+                color = Color.DarkGray
             )
         }
     )
