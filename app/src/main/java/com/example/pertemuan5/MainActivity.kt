efc084b4f2aeca31cc1e92242111a16416a3c8d9
msidiq2235 <muhsidiq2005@gmail.com>
2025-11-01 14:39:03 +0700

    memanggil form registrasi di MainActivity.kt

--- app/src/main/java/com/example/pertemuan5/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan5/MainActivity.kt
@@ -3,44 +3,13 @@ package com.example.pertemuan5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pertemuan5.ui.theme.Pertemuan5Theme
+import com.example.pertemuan5.FormRegistrasi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Pertemuan5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FormDataDiri(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            FormRegistrasi()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pertemuan5Theme {
-        Greeting("Android")
     }
 }
